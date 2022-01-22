ac195a10c8b3121e3d9a9fc6284f2dee181b2096
SanAntonik <sanantonik@gmail.com>
2022-01-22 13:41:44 +0200

    added position checking function and tested it for one player

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,8 +1,8 @@
 import numpy as np
 
-matrix = [["X" for x in range(3)] for y in range(3)] 
+matrix = [[" " for x in range(3)] for y in range(3)] 
 
-print(matrix)
+# print(matrix)
 
 
 def grid(matrix, sep="|"):
@@ -18,8 +18,10 @@ def grid(matrix, sep="|"):
 
 print(grid(matrix))
 
+
 # thanks to https://stackoverflow.com/questions/23294658/asking-the-user-for-input-until-they-give-a-valid-response/23294659#23294659
-def get_valid_int_input(prompt):
+# we're looking for an integer from 0 to 2. Other values won't pass.
+def get_valid_input(prompt):
     while True:
         try:
             value = int(input(prompt))
@@ -34,5 +36,39 @@ def get_valid_int_input(prompt):
             break
     return value
 
-p1_x = get_valid_int_input("\nEnter x value: ")
-print(p1_x)
+
+def check_position_availability(x, y):
+    if matrix[x][y] != " ":
+        print("This position is already taken! Choose other values!")
+        x = get_valid_input("\nEnter x value: ")
+        y = get_valid_input("Enter y value: ")
+        return check_position_availability(x, y)
+    return (x, y)
+    
+
+# if "X" in matrix:
+#     print("X is here")
+
+for x in range(0, 10):
+    p1_x, p1_y = check_position_availability(get_valid_input("\nPlayer 1, enter x value: "), get_valid_input("Player 1, enter y value: "))
+    print(p1_x, p1_y)
+    matrix[p1_x][p1_y] = "X"
+    print(grid(matrix))
+
+    p2_x = get_valid_input("\nPlayer 2, enter x value: ")
+    p2_y = get_valid_input("Player 2, enter y value: ")
+    p2_x, p2_y = check_position_availability(p2_x, p2_y)
+    print(p2_x, p2_y)
+    matrix[p2_x][p2_y] = "O"
+    print(grid(matrix))
+
+
+# if "X" in matrix:
+#     print("X is here")
+
+# p2_x = get_valid_input("\nPlayer 2, enter x value: ")
+# p2_y = get_valid_input("Player 2, enter y value: ")
+# print(check_position_availability(p2_x, p2_y))
+# matrix[p2_x][p2_y] = "O"
+
+# print(grid(matrix))
